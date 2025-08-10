4f850c952adca4c32c2a04d17f2b562726eca159
Main-April <main.april.yt@gmail.com>
2025-08-10 21:29:19 +0200

    Update _.js

--- Main/_.js
+++ Main/_.js
@@ -1,126 +1,130 @@
-// Underscore.js version 0.05
-
+// Nanofy.js version 0.10 
 export class $ {
-  constructor(e) {
-    this.e = e;
-  }
-
-  exe(callback) {
-    this.e.forEach(e => {
-      callback(e);
-    });
-  }
-
-  css(attr, value) {
-    if (!value) {
-      if (typeof attr === "object") {
-        this.exe((element) => {
-          Object.entries(attr).forEach(([item, value]) => {
-            element.style[item] = value;
-          })
-        })
-      }
-      return this.e[0] ? getComputedStyle(this.e[0])[attr] : undefined;
-    }
-    this.exe((e) => {
-      e.style[attr] = value;
-    });
-    return this;
-  }
-
-  html(attr, value) {
-    if (value === undefined) {
-      return this.e[0] ? this.e[0][attr] : undefined;
-    }
-    this.exe((e) => {
-      e[attr] = value;
-    });
-    return this;
-  }
-
-  addEvent(event, callback) {
-    this.exe((e) => {
-      e.addEventListener(event, callback);
-    });
-    return this;
-  }
-
-  remEvent(event, callback) {
-    this.exe((e) => {
-      e.removeEventListener(event, callback);
-    });
-    return this;
-  }
-
-  attr(attr, value) {
-    if (!value) {
-      return this.e[0] ? this.e[0].getAttribute(attr) : undefined;
-    }
-    this.exe(element => {
-      element.setAttribute(attr, value);
-    });
-    return this;
-  }
-
-  click(callback) {
-    this.exe(element => {
-      element.addEventListener("click",callback);
-    });
-    return this;
-  }
-
-  toggle(s) {
-    if (!s) {
-      this.exe((element) => {
-        element.style.display = getComputedStyle(element).display == "none" ? "block" : "none";
-      });
-    } 
-    else {
-      this.exe((element) => {
-        element.style.display = getComputedStyle(element).display == "none" ? s : "none";
-      });
-    }
-    return this;
-  }
- replace(e){
-  if(!e){
-   this.exe((element) => {
-    element.remove();
-   });
+  constructor(e) {
+    this.e = Array.isArray(e) ? e : [e]; 
   }
-  this.exe((element)=>{
-   element.parentNode.replaceChild(element,e);
-  });
-  return this;
-}
     
+  exe(callback) {
+    this.e.forEach(e => {
+      callback(e);
+    });
+  }
+    
+  css(attr, value) {
+    if (!value) {
+      if (typeof attr === "object") {
+        this.exe((element) => {
+          Object.entries(attr).forEach(([item, value]) => {
+            element.style[item] = value;
+          })
+        })
+        return this;
+      }
+      return this.e[0] ? getComputedStyle(this.e[0])[attr] : undefined;
+    }
+    this.exe((e) => {
+      e.style[attr] = value;
+    });
+    return this;
+  }
+    
+  html(attr, value) {
+    if (value === undefined) {
+      return this.e[0] ? this.e[0][attr] : undefined;
+    }
+    this.exe((e) => {
+      e[attr] = value;
+    });
+    return this;
+  }
+    
+  addEvent(event, callback) {
+    this.exe((e) => {
+      e.addEventListener(event, callback);
+    });
+    return this;
+  }
+    
+  remEvent(event, callback) {
+    this.exe((e) => {
+      e.removeEventListener(event, callback);
+    });
+    return this;
+  }
+    
+  attr(attr, value) {
+    if (!value) {
+      return this.e[0] ? this.e[0].getAttribute(attr) : undefined;
+    }
+    this.exe(element => {
+      element.setAttribute(attr, value);
+    });
+    return this;
+  }
+    
+  click(callback) {
+    this.exe(element => {
+      element.addEventListener("click", callback);
+    });
+    return this;
+  }
+    
+  toggle(s) {
+    if (!s) {
+      this.exe((element) => {
+        element.style.display = getComputedStyle(element).display == "none" ? "block" : "none";
+      });
+    } else {
+      this.exe((element) => {
+        element.style.display = getComputedStyle(element).display == "none" ? s : "none";
+      });
+    }
+    return this;
+  }
+    
+  replace(e) {
+    if (!e) {
+      this.exe((element) => {
+        element.remove();
+      });
+    } else {
+      this.exe((element) => {
+        element.parentNode.replaceChild(e, element);
+      });
+    }
+    return this;
+  }
 }
 
-// Setup fonction _
-export function _ (_e_) {
-  let d = new _$_.select(_e_);
-  return new $(d);
-}
 // Setup JS DOM Environment
-export class _$_ {
-    constructor(e){
-    this.e = e;
+export class DOM {
+  constructor() {}
+  
+  select(s) {
+    let i = document.getElementById(s);
+    let c = document.getElementsByClassName(s);
+    let t = document.querySelectorAll(s);
+    if(i||c||t) {
+        return i||Array.from(c||t);
     }
-
-    select(s) {
-    if (document.getElementById(s)) return document.getElementById(s);
-    if (document.getElementsByClassName(s).length > 0) return Array.from(document.getElementsByClassName(s));
-    return Array.from(document.querySelectorAll(s));
-    }
-    repeat(n, c) {
+    throw new Error("[Nanofy.js] The element doesn't exist.");
+  }
+  
+  repeat(n, c) {
     for (let i = 0; i < n; i++) {
-      c();
-        }
+      c(i);
     }
+  }
+}
+
+// Setup fonction _ (CORRIGÉE)
+export function _(s) {
+  return new $(new DOM().select(s));
 }
 
 // Setup global utilisation
-window._ = _;
-window._$_ = _$_;
-window.$ = $;
-
+if (typeof window !== 'undefined') {
+  window._ = _;
+  window.DOM = DOM;
+  window.$ = $;
+}
