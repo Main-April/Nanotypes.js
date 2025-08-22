134ab2d831bffde9033e8e09a1042a889f0ba6d2
Main-April <main.april.yt@gmail.com>
2025-08-22 12:47:03 +0200

    Update string.js

--- Main/string.js
+++ Main/string.js
@@ -1,6 +1,9 @@
-String.prototype.for = function (callback) {
+String.prototype.for = function (c) {
   for (let i = 0; i < this.length; i++) {
-    callback(this[i], i);
+    c(this[i],i);      
   }
 };
 
+String.prototype.occurence = function (o) {
+  return this.split(o).length--;
+}
