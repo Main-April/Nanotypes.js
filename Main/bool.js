45df7a1240d153b68991032df8eccd5db6c75038
Main-April <main.april.yt@gmail.com>
2025-09-25 14:41:11 +0200

    Update bool.js

--- Main/bool.js
+++ Main/bool.js
@@ -1,18 +1,67 @@
+// Main File:
+// Author : Main-April
+// Last update : 25/09/2025
 
-Boolean.prototype.link = function (n) {
-  globalThis[n] = this.valueOf();
-  return this;
+function select(s) {
+    let i = document.getElementById(s);
+    let c = document.getElementsByClassName(s)
+    if (i) return i;
+    if (c && c.length > 0) return c;
+    return document.querySelector(s)
 }
 
-Boolean.prototype.when = function (c) {
-  if(this.valueOf()){
-    c();
-  }
-  return this;
+function css(e, s, v = undefined) {
+    if (type(e) === "string") e = select(e);
+    if (!e) return e.style[s];
+    e.style[s] = v;
 }
 
-Object.defineProperty(Boolean.prototype, "toggle", {
-    set(){
-        this.valueOf() = !this.valueOf();
+// Global JS functions
+function repeat(n, c) {
+    if (type(n) != "number") n = n.length
+    for (let i = 0; i < n; i++) {
+        c(i);
     }
-});
+}
+
+function isin(e, s) {
+    if (type(s) === "object" || type(s) === "array") {
+        if (s.index(e) != -1) return true
+    }
+    if (type(s) === "string") {
+        if (s.includes(e)) return true
+    }
+    return false;
+}
+
+function type(e, t) {
+    e = Object.prototype.toString.call(e).slice(8, -1).toLowerCase();
+    if (!t) return e;
+    return e === t.toLowerCase();
+}
+
+function max(a) {
+    let m;
+    if (type(a) === "array") {
+        m = a[0];
+        a.each((i) => { if (a[i] > m) m = a[i]; });
+    }
+    if (type(a) === "object") {
+        m = Object.values(a)[0];
+        Object.values(a).forEach((i) => { if (i > m) m = i; });
+    }
+    return m;
+}
+
+function min(a) {
+    let m;
+    if (type(a) === "array") {
+        m = a[0];
+        a.each((i) => { if (a[i] < m) m = a[i]; });
+    }
+    if (type(a) === "object") {
+        m = Object.values(a)[0];
+        Object.values(a).forEach((i) => { if (i < m) m = i; });
+    }
+    return m;
+}
