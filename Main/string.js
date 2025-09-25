38ceacbe1ed85ff8d3a688fa8b02a73e6e8cbe50
Main-April <main.april.yt@gmail.com>
2025-09-25 14:02:33 +0200

    Update string.js

--- Main/string.js
+++ Main/string.js
@@ -1,9 +1,33 @@
-String.prototype.for = function (c) {
-  for (let i = 0; i < this.length; i++) {
-    c(this[i],i);      
-  }
+// Array plugin : 
+// Author : Main-April
+// Last update : 24/09/2025
+console.log("Loading Nanotypes.js String module...");
+
+String.prototype.each = function(c) {
+    for (let i = 0; i < this.length; i++) {
+        c(i);
+    }
 };
 
-String.prototype.occurence = function (o) {
-  return this.split(o).length-1;
+String.prototype.reverse = function() {
+    return this.split(" ").reverse().join(" ");
 }
+
+String.prototype.occurenceOf = function(s) {
+    let n = 0;
+    this.each((i) => { if (this[i] === s) n++; });
+    return n;
+}
+
+String.prototype.compareTo = function(s) {
+    s = String(s).substring(0, this.length);
+    let n = 0;
+    this.each((i) => { if (this[i] === s[i]) n++; });
+    return n / this.length;
+}
+
+Object.defineProperty(String.prototype, "isEmpty", {
+    get() {
+        return this.trim().length === 0;
+    }
+});
